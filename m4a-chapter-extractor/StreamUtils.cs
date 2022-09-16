54d0ccea2c0bd1e7374953af47a18852458522e3
jingking <github@jingking.net>
2022-09-16 01:05:43 -0400

    initial upload

---
+++ m4a-chapter-extractor/StreamUtils.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace M4a_chapter_extractor
+{
+	internal class StreamUtils
+	{
+
+
+		public static int GetAtomSize(Stream s)
+		{
+			return StreamUtils.ReadInt32BigEndian(s);
+		}
+
+		public static string GetAtomType(Stream s)
+		{
+			return StreamUtils.ReadShortString(s, 4);
+		}
+
+
+		//parses ('stts') time list
+		public static int[] GetTimeToSample(Stream s)
+		{
+			return StreamUtils.ReadList(s, false, true);
+		}
+
+		//parses ('stsz') size list
+		//public static int[] GetSampleSize(Stream s)
+		//{
+		//	return StreamUtils.ReadList(s, true, false);
+		//}
+
+		//parses ('stco') offset list
+		public static int[] GetChunkOffset(Stream s)
+		{
+			return StreamUtils.ReadList(s);
+		}
+
+		public static string GetChapter(Stream s)
+		{
+			UInt16 len = StreamUtils.ReadUInt16BigEndian(s);
+			if (len > 0)
+				return StreamUtils.ReadShortString(s, len);
+			else
+				return "";
+		}
+
+
+		private static int[] ReadList(Stream s, bool checksize = false, bool checkcount = false)
+		{
+			int[] result = null;
+
+			int version;
+			version = StreamUtils.ReadInt32BigEndian(s);
+
+			if (checksize)
+			{
+				int sample_size;
+				sample_size = StreamUtils.ReadInt32BigEndian(s);
+			}
+
+			int entry_count;
+			entry_count = StreamUtils.ReadInt32BigEndian(s);
+
+
+			if (entry_count > 0 && version == 0)
+			{
+				result = new int[entry_count];
+				for (var i = 0; i < entry_count; i++)
+				{
+					if (checkcount)
+					{
+						var samplecount = StreamUtils.ReadInt32BigEndian(s);
+						if (samplecount == 1)
+							result[i] = StreamUtils.ReadInt32BigEndian(s);
+					}
+					else
+						result[i] = StreamUtils.ReadInt32BigEndian(s);
+				}
+			}
+			return result;
+		}
+
+
+
+		private static long ReadUInt64asLong(Stream s)
+		{
+			return (long)ReadUInt64(s);
+		}
+
+		private static UInt64 ReadUInt64(Stream s)
+		{
+			byte[] buff = new byte[8];
+			s.Read(buff, 0, buff.Length);
+			return BitConverter.ToUInt64(buff, 0);
+		}
+
+		private static int ReadUInt32asInt(Stream s)
+		{
+			return (int)ReadUInt32(s);
+		}
+
+		private static UInt32 ReadUInt32(Stream s)
+		{
+			byte[] buff = new byte[4];
+			s.Read(buff, 0, buff.Length);
+			return BitConverter.ToUInt32(buff, 0);
+		}
+
+		private static Int32 ReadInt32BigEndian(Stream s)
+		{
+			byte[] buff = new byte[4];
+			s.Read(buff, 0, buff.Length);
+			if (BitConverter.IsLittleEndian)
+				Array.Reverse(buff);
+			return BitConverter.ToInt32(buff, 0);
+		}
+
+		private static UInt16 ReadUInt16(Stream s)
+		{
+			byte[] buff = new byte[2];
+			s.Read(buff, 0, buff.Length);
+			return BitConverter.ToUInt16(buff, 0);
+		}
+
+		private static UInt16 ReadUInt16BigEndian(Stream s)
+		{
+			byte[] buff = new byte[2];
+			s.Read(buff, 0, buff.Length);
+			if (BitConverter.IsLittleEndian)
+				Array.Reverse(buff);
+			return BitConverter.ToUInt16(buff, 0);
+		}
+
+		private static string ReadShortString(Stream s, int len)
+		{
+			byte[] buff = new byte[len];
+			s.Read(buff, 0, buff.Length);
+			for (int i = 0; i < len; i++)
+			{
+				if (buff[i] < 32 && buff[i] != 10 && buff[i] != 13)
+					buff[i] = (byte)'?';
+			}
+			return Encoding.ASCII.GetString(buff);
+		}
+	}
+}
---
+++ m4a-chapter-extractor/chapter_extractor.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+
+
+namespace M4a_chapter_extractor
+{
+
+    class chapter_extractor
+    {
+        static void Main(string[] args)
+        {
+            
+            string filepath = "";
+
+            if (args.Length > 0)
+                filepath = args[0];
+            else
+			{
+                Console.Write("Please provide a valid file path.");
+				return;
+			}
+
+
+            try
+            {// Read file
+                using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+                {
+
+                    long MaxLength = (int)fs.Length;
+
+                    if (fs.Length > 0)
+                    {
+                        int[] timetosample = null;
+                        //int[] samplesize; //= null;
+                        int[] chunkoffset = null;
+
+                        long start = 0;
+                        fs.Seek(start, SeekOrigin.Begin);
+                        int len = StreamUtils.GetAtomSize(fs);
+                        fs.Seek(4, SeekOrigin.Current);
+                        var ftyp = StreamUtils.GetAtomType(fs);
+
+                        switch (ftyp)
+                        {
+                            case "m4a ":
+                            case "M4A ":
+                            case "mp42":
+                                ftyp = "m4a";
+                                break;
+                            default:
+                                Console.WriteLine("Not designed for this file type.");
+                                return;
+                        }
+
+
+                        if (!string.IsNullOrEmpty(ftyp))
+                        {
+                            fs.Seek(len - 12, SeekOrigin.Current);//move pointer to the next atom box
+
+                            bool done = false;
+                            int trakcount = 0;
+                            while (!done && fs.Position < fs.Length)
+                            {
+                                len = StreamUtils.GetAtomSize(fs);
+                                string type = StreamUtils.GetAtomType(fs);
+
+                                switch (type)
+                                {
+                                    case "stts"://time   
+                                        timetosample = StreamUtils.GetTimeToSample(fs);
+                                        break;
+
+                                    //case "stsz"://size
+                                    //    samplesize = StreamUtils.GetSampleSize(fs);
+                                    //    break;
+
+                                    case "stco"://offset
+                                        chunkoffset = StreamUtils.GetChunkOffset(fs);
+                                        break;
+
+                                    //move into the following atom boxes
+                                    case "moov":
+                                    case "mdia":
+                                    case "minf":
+                                    case "stbl":
+
+                                        fs.Seek(0, SeekOrigin.Current);
+                                        break;
+
+
+                                    case "trak":
+                                        trakcount++;
+                                        if (trakcount < 2)
+                                            fs.Seek(len - 8, SeekOrigin.Current);
+                                        else
+                                            fs.Seek(0, SeekOrigin.Current);
+                                        break;
+                                    //skip the following atom boxes
+                                    case "free":
+                                    case "mdat":
+                                    case "uuid":
+                                    case "mvhd":
+                                    case "stsc":
+
+                                    default:
+                                        fs.Seek(len - 8, SeekOrigin.Current);
+                                        break;
+
+                                }
+                            }
+
+                            //get chapter strings
+                            if ((timetosample != null && chunkoffset != null) && (timetosample.Length == chunkoffset.Length))
+                            {
+                                Chapter[] chapters = new Chapter[timetosample.Length];
+                                int t = 0;
+                                for (var i = 0; i < chapters.Length; i++)
+                                {
+                                    fs.Position = chunkoffset[i];
+                                    t = timetosample[i] + t;
+                                    chapters[i] = new Chapter(StreamUtils.GetChapter(fs), (i < 1) ? 0 : chapters[i - 1].EndTime,  t/ 1000.0);
+                                    
+                                };
+                                /*
+                                 * output format
+                                 *     chapters: [
+                                                  { title, startTime, endTime }, // the first chapter
+                                                  { title, startTime, endTime }, // the second chatper
+                                                ],
+                                */
+                                Console.WriteLine("[");
+                                for (var i = 0; i < chapters.Length; i++)
+                                {
+
+                                    Console.WriteLine("{\"" + chapters[i].Title + "\"," + chapters[i].StartTime + "," + chapters[i].EndTime + "},");
+
+                                };
+                                Console.WriteLine("]");
+                                Console.ReadLine();
+                            }
+
+                        }
+
+                    }
+
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            // Debug - Dump 16 bytes per line
+            //for (int i = 0; i < len; i += 16)
+            //{
+            //    var cnt = Math.Min(16, len - i);
+            //    var line = new byte[cnt];
+            //    Array.Copy(bits, i, line, 0, cnt);
+            //    // Write address + hex + ascii
+            //    Console.Write("{0:X6}  ", i);
+            //    Console.Write(BitConverter.ToString(line));
+            //    Console.Write("  ");
+            //    // Convert non-ascii characters to .
+            //    for (int j = 0; j < cnt; ++j)
+            //        if (line[j] < 0x20 || line[j] > 0x7f) line[j] = (byte)'.';
+            //    Console.WriteLine(Encoding.ASCII.GetString(line));
+            //}
+        }
+    }
+}
